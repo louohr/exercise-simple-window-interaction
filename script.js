b2ea76caecf2cc5e38d32d5a3af36347922ecb52
Louise Öhrner <101390153+louohr@users.noreply.github.com>
2024-07-05 16:09:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// let userName = prompt("Vad heter du?");
+// console.log(userName);
+
+// let birthYear = parseInt(prompt("Vilket år är du född?"));
+// console.log(birthYear);
+
+// let currentYear = new Date().getFullYear();
+
+// let age = currentYear - birthYear;
+// console.log(age);
+
+let firstNumber = parseInt(prompt("Skriv ett nummer"));
+
+let secondNumber = parseInt(prompt("Skriv ett annat nummer"));
+
+console.log(firstNumber);
+console.log(secondNumber);
+
+let addition = firstNumber + secondNumber;
+let subtraction = firstNumber - secondNumber;
+let multiplication = firstNumber * secondNumber;
+let division = firstNumber / secondNumber;
+
+console.log("Addition: " + addition);
+console.log("Subtraction: " + subtraction);
+console.log("Multiplication: " + multiplication);
+console.log("Division: " + division);
+
+alert("Uträkningarna är nu klara.");
